00ce2d97a78bc221538fd9f6d49790bcda6f5921
csabinyoo <mdgycsba@gmail.com>
2025-01-15 14:11:34 +0100

    asd

--- TelkekProject/Program.cs
+++ TelkekProject/Program.cs
@@ -10,7 +10,7 @@ namespace TelkekProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("tes2zt");
+            Console.WriteLine("");
             Console.ReadLine();
         }
     }
