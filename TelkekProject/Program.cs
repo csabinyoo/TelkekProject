5337313491877c672f61f7cb235b807086332654
csabinyoo <mdgycsba@gmail.com>
2025-01-15 14:10:13 +0100

    *

--- TelkekProject/Program.cs
+++ TelkekProject/Program.cs
@@ -10,7 +10,7 @@ namespace TelkekProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("asdsdsad");
+            Console.WriteLine("teszt");
             Console.ReadLine();
         }
     }
