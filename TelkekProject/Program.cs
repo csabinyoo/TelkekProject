018e885f07a97e7ad2e3adece1b211f0d9608551
csabinyoo <mdgycsba@gmail.com>
2025-01-17 08:44:16 +0100

    *

--- TelkekProject/Program.cs
+++ TelkekProject/Program.cs
@@ -17,8 +17,8 @@ namespace TelkekProject
                 new Négyszög("T3", false, true, 14.24, 26.4, 23.1, 41.9, 27.32),
                 new Háromszög("T4", false, false, 23.1, 18.36, 13.95),
                 new Téglalap("T5", true, true, 13.95, 12.9),
-                new Négyszög("T6", false, true, 13.95, 23.13, 14.48, 19.26, 27),
-                new Háromszög("T7", false, true, 14.48, 22.84, 22.14),
+                new Négyszög("T6", true, true, 13.95, 23.13, 14.48, 19.26, 27),
+                new Háromszög("T7", true, true, 14.48, 22.84, 22.14),
                 new Négyszög("T8", false, false, 22.84, 38, 13.95, 19.71, 24.15),
             };
 
